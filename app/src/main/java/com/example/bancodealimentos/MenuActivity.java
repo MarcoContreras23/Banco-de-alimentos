fa298c6d6106ac19a312e4836ae5f8c5232d2f62
Camilog699 <amraga10@gmail.com>
2020-03-19 08:24:52 -0500

    Logout

--- app/src/main/java/com/example/bancodealimentos/MenuActivity.java
+++ app/src/main/java/com/example/bancodealimentos/MenuActivity.java
@@ -1,13 +1,33 @@
 package com.example.bancodealimentos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
+
+    Button btnOut;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnOut = (Button) findViewById(R.id.logout);
+
+        btnOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

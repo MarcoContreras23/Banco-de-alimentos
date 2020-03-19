327dbca43a7da7e37286e6776f35e26e1b6a4f79
Camilog699 <amraga10@gmail.com>
2020-03-19 18:44:57 -0500

    Project

---
+++ app/src/main/java/com/example/bancodealimentos/HomeActivity.java
@@ -0,0 +1,33 @@
+package com.example.bancodealimentos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btn = (Button) findViewById(R.id.button1);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/bancodealimentos/MenuActivity.java
+++ app/src/main/java/com/example/bancodealimentos/MenuActivity.java
@@ -32,7 +32,7 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(MenuActivity.this, MainActivity.class);
+                Intent i = new Intent(MenuActivity.this, HomeActivity.class);
                 startActivity(i);
             }
         });

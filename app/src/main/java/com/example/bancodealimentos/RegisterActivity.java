1c344e56d5329ebc4c6e835fee81ab49b091d291
Jose Cruz <josedanielcrzgmail.com>
2020-03-19 10:34:01 -0500

    Se comienza a enlazar vista de registro con RegisterActivity

--- app/src/main/java/com/example/bancodealimentos/RegisterActivity.java
+++ app/src/main/java/com/example/bancodealimentos/RegisterActivity.java
@@ -1,14 +1,27 @@
 package com.example.bancodealimentos;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText name;
+    EditText lastName;
+    EditText email;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = (EditText) findViewById(R.id.username);
+        name = (EditText) findViewById(R.id.Name);
+        lastName = (EditText) findViewById(R.id.LastName);
+        email = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
     }
 }
